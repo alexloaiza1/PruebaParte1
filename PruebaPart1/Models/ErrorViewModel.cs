acec4a8f1462ce521b5c7d7317bfec9055cce0ea
Alexander Loaiza <alexloaiza1@gmail.com>
2022-10-11 16:45:45 -0500

    Initial Commit project

---
+++ PruebaPart1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PruebaPart1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
