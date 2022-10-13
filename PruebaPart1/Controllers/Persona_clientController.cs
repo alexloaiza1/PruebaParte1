bc10b9b5bb43435b218680e909d6cf53c971c7dd
Alexander Loaiza <alexloaiza1@gmail.com>
2022-10-13 01:15:34 -0500

    Agrego funcionalidad de genero

--- PruebaPart1/Controllers/Persona_clientController.cs
+++ PruebaPart1/Controllers/Persona_clientController.cs
@@ -25,6 +25,14 @@ namespace PruebaPart1.Controllers
               return View(await _context.Persona_cliente.ToListAsync());
         }
 
+
+        // GET: Persona_client
+        public async Task<IActionResult> Interacciones()
+        {
+            return View(await _context.Persona_cliente.ToListAsync());
+        }
+
+
         // GET: Persona_client/Details/5
         public async Task<IActionResult> Details(int? id)
         {
